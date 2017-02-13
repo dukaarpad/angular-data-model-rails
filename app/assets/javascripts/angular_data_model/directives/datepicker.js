7738be5b159bfbce0fe4f3b1bcddbfa0a82569f7
Duka Arpad <duka.arpad@gmail.com>
2017-02-13 15:34:05 +0100

    datepicker developments

--- app/assets/javascripts/angular_data_model/directives/datepicker.js
+++ app/assets/javascripts/angular_data_model/directives/datepicker.js
@@ -6,17 +6,16 @@
       restrict: 'E',
       template: '<div class="input-group date"><input ng-model="trans_value" class="form-control input-sm"/><label class="input-group-addon"><span class="glyphicon glyphicon-calendar"></span></label></div>',
       scope: {
-        model: '='
+        model: '=',
+        key: '=',
+        noLimit: '=',
+        startDateAttr: '=',
+        endDateAttr: '=',
+        fromNow: '=',
+        allowClear: '=',
       },
       link: function(scope, el, attr) {
-        var startDate = el.attr('start-date'),
-        endDate = el.attr('end-date'),
-        key = attr['key'],
-        from_now = el.attr('from-now'),
-        no_limit = el.attr('no-limit'),
-        allowClear = el.attr('allow-clear');
-
-        scope.$watch('model[\'' + key + '\']', function(nv){
+        scope.$watch('model[\'' + scope.key + '\']', function(nv){
           scope.trans_value = c.getDate(nv);
         });
 
@@ -29,8 +28,8 @@
           weekStart: 1,
         });
 
-        if(!no_limit) {
-          if(from_now)
+        if(!scope.noLimit) {
+          if(scope.fromNow)
             input.datepicker('setStartDate', new Date());
           else
             input.datepicker('setEndDate', new Date());
@@ -45,20 +44,22 @@
             if (val) input.datepicker('update', val);
           });
 
-        scope.$watchCollection('[model[\'' + startDate + '\'], model[\'' + endDate + '\']];', function(val) {
-          if (val[0])
-            input.datepicker('setStartDate', new Date(val[0]))
-          else if (val[1])
-            input.datepicker('setEndDate', new Date(val[1]));
-          el.data('nofocus', 2);
-        }, true);
+        if(scope.startDateAttr || scope.startEndAttr) {
+          scope.$watchCollection('[model[\'' + scope.startDateAttr + '\'], model[\'' + scope.endDateAttr + '\']];', function(val) {
+            if (val[0])
+              input.datepicker('setStartDate', new Date(val[0]))
+            else if (val[1])
+              input.datepicker('setEndDate', new Date(val[1]));
+            el.data('nofocus', 2);
+          }, true);
+        }
 
         input.on('hide', function(e) {
-          if (input.val() === '' && allowClear == 'false' )
+          if (input.val() === '' && scope.allowClear == 'false' )
             input.datepicker('update', c.getDate(scope.trans_value));
-          else if(c.getDate(scope.trans_value) != c.getDate(scope.model[key])) {
+          else if(c.getDate(scope.trans_value) != c.getDate(scope.model[scope.key])) {
             scope.trans_value = c.getDate(scope.trans_value);
-            scope.model[key] = scope.trans_value;
+            scope.model[scope.key] = scope.trans_value;
           }
           scope.$apply();
         });
--- app/assets/javascripts/angular_data_model/model_editors/date.js
+++ app/assets/javascripts/angular_data_model/model_editors/date.js
@@ -15,6 +15,15 @@
         return {
           pre: function preLink(scope, element, attributes) {
             helpers.parse_object_attribute(scope, attributes['dateField'], attributes);
+            if(scope.object && scope.object.constructor && scope.object.constructor.$behaviors && scope.object.constructor.$behaviors[scope.attribute]) {
+              var settings = scope.object.constructor.$behaviors[scope.attribute];
+              scope.no_limit = true;
+              [ 'start_date_attr', 'end_date_attr', 'no_limit', 'from_now', 'allow_clear' ].each(function(set_attr) {
+                if(Object.keys(settings).indexOf(set_attr) != -1) {
+                  scope[set_attr] = settings[set_attr];
+                }
+              })
+            }
           }
         }
       },
