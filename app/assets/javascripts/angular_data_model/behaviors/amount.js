fc2a4ba6029b0ea5749723c2ad262ec9ff3cb27a
Duka Arpad <duka.arpad@gmail.com>
2017-06-15 16:48:57 +0200

    Amount handling fix

--- app/assets/javascripts/angular_data_model/behaviors/amount.js
+++ app/assets/javascripts/angular_data_model/behaviors/amount.js
@@ -23,6 +23,7 @@
       if(!Model.prototype.$watchers) Model.prototype.$watchers = {};
       Model.prototype.$watchers[attribute] = Model.prototype.$watchers[attribute] ? Model.prototype.$watchers[attribute].clone() : [];
       Model.prototype.$watchers[attribute].push(function(){
+        if(this[attribute] == null) return;
         var fixed = + parseFloat(this[attribute] || 0).toFixed(this[attribute + '_precision'] || precision);
         if(this[attribute] != fixed) this[attribute] = fixed;
       })
